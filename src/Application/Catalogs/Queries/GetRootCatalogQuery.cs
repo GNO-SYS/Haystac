7251de9a7c548c721432057c2b5035b37827e9c8
Graham Clodge <graham.clodge@gmail.com>
2023-08-16 16:29:02 -0400

    Adding '/' route and initial 'root' catalog handler for #20

---
+++ src/Application/Catalogs/Queries/GetRootCatalogQuery.cs
@@ -0,0 +1,28 @@
+﻿namespace Haystac.Application.Catalogs.Queries;
+
+public record GetRootCatalogQuery : IRequest<CollectionDto> { }
+
+public class GetRootCatalogQueryHandler
+    : IRequestHandler<GetRootCatalogQuery, CollectionDto>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetRootCatalogQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<CollectionDto> Handle(GetRootCatalogQuery query, CancellationToken cancellationToken)
+    {
+        var collecs = await _context.Collections.ToListAsync(cancellationToken);
+
+        var dto = new CollectionDto
+        {
+            Type = "Catalog"
+        };
+
+        //< TODO - Populate the Catalog with the above & any routing configuration
+
+        return dto;
+    }
+}
---
+++ src/WebApi/Controllers/RootController.cs
@@ -0,0 +1,22 @@
+﻿using Haystac.Application.Catalogs.Queries;
+
+namespace Haystac.WebApi.Controllers;
+
+[Route("/")]
+[ApiController]
+public class RootController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public RootController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<CollectionDto>> GetRootCatalog()
+        => await _mediator.Send(new GetRootCatalogQuery());
+
+    //< TODO - Actually complete the 'GetRootCatalogQueryHandler'
+    //< TODO - Add the [HttpGet("api")] route & handler
+}
