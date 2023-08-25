9582980498bc79e594ff27aa3fff2ce03d7b84f2
Graham Clodge <graham.clodge@gmail.com>
2023-08-24 21:43:23 -0400

    Filled out root catalog query, added required link generation

--- src/Application/Catalogs/Queries/GetRootCatalogQuery.cs
+++ src/Application/Catalogs/Queries/GetRootCatalogQuery.cs
@@ -1,4 +1,6 @@
-﻿namespace Haystac.Application.Catalogs.Queries;
+﻿using Haystac.Domain.ValueObjects;
+
+namespace Haystac.Application.Catalogs.Queries;
 
 public record GetRootCatalogQuery : IRequest<CollectionDto> { }
 
@@ -6,23 +8,71 @@ public class GetRootCatalogQueryHandler
     : IRequestHandler<GetRootCatalogQuery, CollectionDto>
 {
     private readonly IApplicationDbContext _context;
+    private readonly IUrlService _url;
 
-    public GetRootCatalogQueryHandler(IApplicationDbContext context)
+    public GetRootCatalogQueryHandler(IApplicationDbContext context,
+                                      IUrlService url)
     {
         _context = context;
+        _url = url;
     }
 
     public async Task<CollectionDto> Handle(GetRootCatalogQuery query, CancellationToken cancellationToken)
     {
-        var collecs = await _context.Collections.ToListAsync(cancellationToken);
+        //< Get only the 'anonymous' collections
+        var collecs = await _context.Collections
+                                    .Where(c => string.IsNullOrEmpty(c.ClientId))
+                                    .ToListAsync(cancellationToken);
+
+        var links = await GenerateLinks(collecs);
 
         var dto = new CollectionDto
         {
-            Type = "Catalog"
+            StacVersion = "1.0.0",
+            Identifier = "haystac-root",
+            Title = "Root catalog for Haytstac API",
+            Type = "Catalog",
+            Links = links
         };
 
-        //< TODO - Populate the Catalog with the above & any routing configuration
+        //< TODO - Handle 'Conforms to' tags - where is that?
+        //< TODO - How handle Identifier / Title / Description / version, etc?
+        //<         .. We may need to store a 'root' Catalog, and make this one on-demand if needed
+        //<      - Leaning on the side of maintaing a 'haystac-root' catalog, that is seeded as above
+        //<      - Users can then update it as they see fit, we don't have to manage it all
 
         return dto;
+    }
+
+    Task<List<Link>> GenerateLinks(IEnumerable<Collection> collecs)
+    {
+        var baseUrl = _url.GetBaseUrl();
+
+        var links = new List<Link>
+        {
+            new Link
+            {
+                Relationship = "root",
+                Href = $"{baseUrl}",
+                Type = "application/json"
+            },
+            new Link
+            {
+                Relationship = "self",
+                Href = $"{baseUrl}",
+                Type = "application/json"
+            },
+            new Link
+            {
+                Relationship = "service-desc",
+                Href = $"{baseUrl}/swagger/v1/swagger.json",
+                Type = "application/vnd.oai.openapi+json;version=3.0"
+            }
+        };
+
+        var collectionLinks = collecs.Select(Link.GenerateChildLink);
+        links.AddRange(collectionLinks);
+
+        return Task.FromResult(links);
     }
 }
