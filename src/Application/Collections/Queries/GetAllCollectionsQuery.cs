b2b9ea91f6ef8d9234c76e2725ff6dcbf840b6a3
Graham Clodge <graham.clodge@gmail.com>
2023-09-06 13:46:17 -0400

    Added actual filtration to the 'GetAllCollectionsQuery'

--- src/Application/Collections/Queries/GetAllCollectionsQuery.cs
+++ src/Application/Collections/Queries/GetAllCollectionsQuery.cs
@@ -6,14 +6,23 @@ public class GetAllCollectionsQueryHandler
     : IRequestHandler<GetAllCollectionsQuery, List<CollectionDto>> 
 {
     private readonly IApplicationDbContext _context;
+    private readonly IClientFilter _filter;
 
-    public GetAllCollectionsQueryHandler(IApplicationDbContext context)
+    public GetAllCollectionsQueryHandler(
+        IApplicationDbContext context,
+        IClientFilter filter)
     {
         _context = context;
+        _filter = filter;
     }
 
-    public async Task<List<CollectionDto>> Handle(GetAllCollectionsQuery request, CancellationToken cancellationToken)
-        => await _context.Collections
-                         .Select(c => c.ToDto())
-                         .ToListAsync(cancellationToken);
+    public async Task<List<CollectionDto>> Handle(GetAllCollectionsQuery request,
+        CancellationToken cancellationToken)
+    {
+        var collecs = await _context.Collections.ToListAsync(cancellationToken);
+
+        var filtered = await _filter.FilterAsync(collecs);
+
+        return filtered.Select(c => c.ToDto()).ToList();
+    }
 }
